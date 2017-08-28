ab7964e33ff5ed1dcce51adcdddf365cbbca7bfa
Chris Underwood <cunderwoodmn@gmail.com>
2017-08-28 01:15:32 -0700

    create basic calculator functionality

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 (function(){
     // all your code here
+    function Calculator (accum) {
+      this.accum = 0;
+      this.number = number;
 
+      this.addition = function(number){
+        this.accum = this.accum + number;
+        return this.accum;
+      };
 
-    
+      this.multiply = function(number){
+        this.accum = this.accum * number;
+        return this.accum;
+      };
+
+      this.subtract = function(number) {
+        this.accum = this.accum - number;
+        return this.accum;
+      };
+
+      this.divide = function(number) {
+        this.accum = this.accum / number;
+        return this.accum;
+      };
+
+    }
+
+    //var calc = new Calculator();
 })();
