69ecbf157a273772c5eaf6e767cbf5d77b7c3a0e
Chris Underwood <cunderwoodmn@gmail.com>
2017-08-29 00:55:06 -0700

    add code for grabbing event and determining which one was selected

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+//'use esversion: 6'
+
 (function(){
+  'use strict';
     // all your code here
     function Calculator (accum) {
       this.accum = 0;
@@ -24,7 +27,47 @@
         return this.accum;
       };
 
+      this.clearAccum = function(accum) {
+        this.accum = 0;
+        return this.accum;
+      };
+
     }
+
+    document.getElementById("buttonList").addEventListener("click", function(e) {
+
+    	if(e.target && e.target.nodeName == "BUTTON") {
+    		// List item found!  Output the ID!
+        console.log("e.target.nodeName   ", e.target.nodeName);
+    		console.log("Button ", e.target.id, " was clicked!");
+        let buttonClick = Number(e.target.id);
+        if ( isNaN(buttonClick )) {
+          buttonClick = e.target.id;
+        }
+        console.log("buttonClick  ", buttonClick);
+        console.log("typeof buttonClick ", typeof buttonClick);
+          if ( buttonClick >= 0 && buttonClick <= 9) {
+            console.log("it's a number");
+          } else if (buttonClick === "+"){
+            console.log("it's a +");
+          } else if (buttonClick === "-"){
+            console.log("it's a -");
+          } else if (buttonClick === "x"){
+            console.log("it's a *");
+          } else if (buttonClick === "/"){
+            console.log("it's a /");
+          } else if (buttonClick === "C"){
+            console.log("it's a C");
+          } else if (buttonClick === "="){
+            console.log("it's a =");
+          } else if (buttonClick === "."){
+            console.log("it's a .");
+          }
+            else {
+            console.log("not sure what it is");
+          }
+        }
+    });
 
     //var calc = new Calculator();
 })();
